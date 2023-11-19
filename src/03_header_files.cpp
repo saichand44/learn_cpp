90518692cea3ac27edf16763b7d085f279a4274e
saichand44 <saichandgubbala@gmail.com>
2023-11-19 17:44:56 -0500

    progress

--- src/03_header_files.cpp
+++ src/03_header_files.cpp
@@ -5,12 +5,16 @@
 
 // Here <> brackets are used instead of the "", as this is just saying the compiler
 // to include the files that are in the include path list (similar to sys path in
-// python). This is sued for standard include files. Also C++ standard header
+// python). This is used for standard include files. Also C++ standard header
 // files do not have the extension ".h" but C standard header files do! (like stdlib.h)
 
 #include "../include/log.h"
-// Here "" are used instead of the <> brackets as we are saying the compuler to
-// includes the files that are in the desired (current / relative) folder
+// Here "" are used instead of the <> brackets as we are saying the compiler to
+// include the files that are in the desired (current / relative) folder. Using
+// the entire relative path might not be entirely necessary as we can just write
+// instructions to cmake where to search for the header files. In this case, 
+// we are using the default compiler instrcutions from vscode, so included the 
+// relative path
 
 int main()
 {
